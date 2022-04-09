9cdd72ddd7a7840e740f3636729d480a554b535a
LukaFlores <luka.flores@mail.com>
2022-04-08 23:36:14 -0700

    chore: change versions of webpack container & marketing

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
-const commonConfig = require('./webpack.common');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
+const commonConfig = require('./webpack.common');
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/marketing/latest/'
+    publicPath: '/marketing/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
