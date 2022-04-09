3fefa349febf1f07eabd8fb4442ae78d9e3d5d39
LukaFlores <luka.flores@mail.com>
2022-04-09 11:16:57 -0700

    feature(auth): add auth to container & deployment auth

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { createGenerateClassName, StylesProvider } from '@material-ui/core';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
@@ -8,6 +8,8 @@ const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 export default () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
   const generateClassName = createGenerateClassName({
     productionPrefix: 'co',
   });
@@ -16,10 +18,12 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header />
+          <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
           <Suspense fallback={<Progress />}>
             <Switch>
-              <Route path="/auth" component={AuthLazy} />
+              <Route path="/auth">
+                <AuthLazy onSignIn={() => setIsSignedIn(true)} />
+              </Route>
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
